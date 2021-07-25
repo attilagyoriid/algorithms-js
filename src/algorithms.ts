d692e2904c23f1d3734e18e1462b1a51bb85c74f
Attila Gyori <attila.gyori.id@gmail.com>
2021-07-25 19:58:37 +0200

    method signature change for typed

--- src/algorithms.ts
+++ src/algorithms.ts
@@ -1,7 +1,15 @@
 
+interface findCommon {
+    (arg0: string[], arg1: string[]): boolean;
+  }
+  interface findSumPair {
+    (arg0: number[], arg1: number): boolean;
+  }
+
+
 class Algorithm {
 
-    public findCommonElements: (arg0: string[], arg1: string[]) => boolean = (list1: string[] = [], list2: string[] = []) => {
+    public findCommonElements: findCommon = (list1: string[] = [], list2: string[] = []) => {
         for (let i = 0; i < list1.length; i++) {
 
             if (list2.includes(list1[i])) {
@@ -12,9 +20,9 @@ class Algorithm {
         return false;
     }
 
-    public findCommonElements2: (arg0: string[], arg1: string[]) => boolean = (list1: string[] = [], list2: string[] = []) => list1.some(item => list2.includes(item));
+    public findCommonElements2: findCommon = (list1: string[] = [], list2: string[] = []) => list1.some(item => list2.includes(item));
 
-    public findCommonElements3: (arg0: string[], arg1: string[]) => boolean = (list1: string[] = [], list2: string[] = []) => {
+    public findCommonElements3: findCommon = (list1: string[] = [], list2: string[] = []) => {
         const set = new Set();
 
         list1.some(item => list2.includes(item));
@@ -22,7 +30,7 @@ class Algorithm {
     }
 
 
-    public hasPairInListWithSum: (arg0: number[], arg1: number) => boolean = (list: number[] = [], sum: number) => {
+    public hasPairInListWithSum(list: number[] = [], sum: number): boolean {
         const mySet = new Set();
         const length = list.length;
         for (let i = 0; i < length; i++) {
